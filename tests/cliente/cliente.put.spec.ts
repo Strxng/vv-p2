9691cb05c54b1e45c7c3fd82c145ce98ef9e3116
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-11-22 23:44:56 -0300

    cliente post and put

--- tests/cliente/cliente.put.spec.ts
+++ tests/cliente/cliente.put.spec.ts
@@ -3,14 +3,23 @@ import supertest from 'supertest';
 
 const domain = 'https://tester-global-cliente-api.herokuapp.com'
 
-describe('Integration tests for cliente api endpoint using post method', () => {
+describe('Integration tests for cliente api endpoint using put method', () => {
   it('should returns status 400 when is called with an empty body', async () => {
-    const { status } = await supertest(domain).post('/cliente').send({})
+    const { status } = await supertest(domain).put('/cliente').send({})
     expect(status).toBe(400)
   })
 
+  it('should returns status 404 when is called without id', async () => {
+    const { status } = await supertest(domain).put('/cliente').send({
+      nome: 'Leonardo Bazan',
+      idade: 21,
+      risco: 1
+    })
+    expect(status).toBe(404)
+  })
+
   it('should returns an error object when is called with an empty body', async () => {
-    const { body } = await supertest(domain).post('/cliente').send({})
+    const { body } = await supertest(domain).put('/cliente').send({})
     const errorProps = Object.keys(body)
 
     expect(errorProps).contains('timestamp')
@@ -21,14 +30,14 @@ describe('Integration tests for cliente api endpoint using post method', () => {
   })
 
   it('should returns status 201 when is called with a correct body', async () => {
-    const { status } = await supertest(domain).post('/cliente').send({
+    const { status } = await supertest(domain).put('/cliente').send({
       id: 1,
       nome: 'Leonardo Bazan',
       idade: 21,
       risco: 1
     })
 
-    expect(status).toBe(201)
+    expect(status).toBe(200)
   })
 
   it('should returns an object with clients containing the client sended when is called with a correct body', async () => {
@@ -39,7 +48,7 @@ describe('Integration tests for cliente api endpoint using post method', () => {
       risco: 1
     }
 
-    const { body } = await supertest(domain).post('/cliente').send(client)
+    const { body } = await supertest(domain).put('/cliente').send(client)
 
     const insertedClient = body[client.id]
     expect(insertedClient).toEqual(client)
